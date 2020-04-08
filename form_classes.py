baa19a74ffe12586c37f9d11a5eea7551cbe1376
codeC4t <49525233+codeC4t@users.noreply.github.com>
2020-04-08 15:56:19 +0400

    update

--- form_classes.py
+++ form_classes.py
@@ -4,6 +4,8 @@ from PyQt5.QtGui import *
 from fmain import Ui_fmain
 from consts import *
 import requests
+import os
+import geocoder
 from functools import partial
 from io import BytesIO
 
@@ -17,7 +19,7 @@ class MainForm(QMainWindow, Ui_fmain):
     def updateMapType(self, Mtype, map_file):
         self.mapType = Mtype
         self.map_file = map_file
-        self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.isPoint)
+        self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.CurrentPoints)
 
     def deletePoint(self):
         self.isPoint = False
@@ -25,75 +27,92 @@ class MainForm(QMainWindow, Ui_fmain):
         self.LELSearch.clear()
         self.LEAddress.clear()
         self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.isPoint)
+    
+    def updateSearch(self): 
+        self.isSearchNearMe = not(self.isSearchNearMe)
 
     def config(self):
         self.BSearch.clicked.connect(self.start_search)
         self.BCancel.clicked.connect(self.deletePoint)
         self.BSearch.setAutoDefault(True)
+        self.isSearchNearMe = False
+        self.RBSearchNearMe.toggled.connect(self.updateSearch)
         self.LELSearch.returnPressed.connect(self.BSearch.click)
         self.spn = 0.01
-        self.isPoint = False
-        self.CurrentPoint = None
+        self.CurrentPoints = []
         self.installEventFilter(self)
-        self.CurrentLattitude = 53.2
-        self.CurrentLongitude = 50.15
+        self.CurrentLattitude, self.CurrentLongitude = geocoder.ip('me').latlng
         self.mapType = 'map'
         self.map_file = 'map.png'
         self.BMap.clicked.connect(partial(self.updateMapType, Mtype='map', map_file='map.png'))
         self.BSat.clicked.connect(partial(self.updateMapType, Mtype='sat', map_file='map.jpg'))
         self.BSkl.clicked.connect(partial(self.updateMapType, Mtype='sat,skl', map_file='map.jpg'))
-        self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, False)
+        self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude)
     
     def eventFilter(self, obj, event):
         if event.type() == QEvent.KeyPress and event.key() == Qt.Key_PageDown:
             if self.spn > 0.0001:
                 self.spn /= 10
-                self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.isPoint) 
+                self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.CurrentPoints) 
         elif event.type() == QEvent.KeyPress and event.key() == Qt.Key_PageUp:
             if self.spn < 50:
                 if self.spn >= 10:
                     self.spn *= 2
                 else:
                     self.spn *= 10
-                self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.isPoint)
+                self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.CurrentPoints) 
         elif event.type() == QEvent.KeyPress and event.key() == Qt.Key_Up:
             if self.CurrentLattitude + self.spn < 90:
                 self.CurrentLattitude += self.spn
-            self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.isPoint)
+            self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.CurrentPoints) 
         elif event.type() == QEvent.KeyPress and event.key() == Qt.Key_Down:
             if self.CurrentLattitude - self.spn > -90:
                 self.CurrentLattitude -= self.spn
-            self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.isPoint)
+            self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.CurrentPoints) 
         elif event.type() == QEvent.KeyPress and event.key() == Qt.Key_Right:
             if self.CurrentLongitude + self.spn < 180:
                 self.CurrentLongitude += self.spn
-            self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.isPoint)
+            self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.CurrentPoints) 
         elif event.type() == QEvent.KeyPress and event.key() == Qt.Key_Left:
             if self.CurrentLongitude - self.spn > -180:
                 self.CurrentLongitude -= self.spn
-            self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.isPoint)
+            self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, self.CurrentPoints)
+        elif event.type() == QEvent.MouseButtonPress and event.button() == Qt.LeftButton:
+            x, y = event.pos().x(), event.pos().y()
+            if x >= 210:
+                spnPpix = self.spn / 450
+                delta_x = x - 435 
+                delta_y = y - 225
+                lattitude = delta_y * spnPpix
+                longitude = delta_x * spnPpix
+                print(f'spnH={spnPpix}')
+                print(f'spnW={spnPpix}')
+                print(f'delta_x={delta_x}')
+                print(f'delta_y={delta_y}')
+                print(f'lattitude={lattitude}')
+                print(f'longitude={longitude}')
+                print(f'CurLong={self.CurrentLongitude}')
+                print(f'CurLat={self.CurrentLattitude}')
+                print('-------------------')
+                self.CurrentLongitude += longitude
+                self.CurrentLattitude -= lattitude
+                self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, [f'{self.CurrentLongitude},{self.CurrentLattitude},pm2bll'])
         return QWidget.eventFilter(self, obj, event)
     
     def start_search(self):
         toponym_to_find = self.LELSearch.text()
-        if self.search_with_toponym(toponym_to_find) is None:
+        if self.search_with_organisation(toponym_to_find) is None:
             self.LELSearch.setText('Ошибка выполнения запроса')
     
-    def search_with_coords(self, longitude, lattitude, point=True, new_point=False):
+    def search_with_coords(self, longitude, lattitude, points=[]):
         self.LELSearch.clearFocus()
         search_params = {
             'll': f'{longitude},{lattitude}',
             'spn': f'{self.spn},{self.spn}',
             'l': self.mapType,
+            'size': '450,450',
+            'pt':'~'.join(points)
         }
-        if point and not new_point:
-            search_params['pt'] = self.CurrentPoint
-        elif point and new_point:
-            self.CurrentPoint =  f'{longitude},{lattitude},pm2bll'
-            search_params['pt'] = self.CurrentPoint
-        else:
-            self.CurrentPoint = None
-        self.isPoint = point
 
         try:
             response = requests.get(map_server, params=search_params)
@@ -102,26 +121,31 @@ class MainForm(QMainWindow, Ui_fmain):
                 file.write(response.content)
         
             self.MAP.setPixmap(QPixmap(self.map_file))
+            os.remove(self.map_file)
+            self.CurrentPoints = points
             return True
         except:
             return None
     
-    def search_with_toponym(self, geocode):
+    def search_with_organisation(self, toponym_to_find):
         search_params = {
-            'geocode': geocode,
-            'apikey': geocoder_apikey,
-            'format': 'json'
+            'apikey': search_apikey,
+            'text': toponym_to_find,
+            'lang': 'ru_RU',
         }
-        
-        try:
-            response = requests.get(geocoder_server, params=search_params)
-            json_response = response.json()
+        if self.isSearchNearMe:
+            search_params['ll'] = ','.join(list(map(str, geocoder.ip('me').latlng[::-1])))
 
-            toponym = json_response['response']['GeoObjectCollection']['featureMember'][0]
-            toponym_address = toponym['GeoObject']['metaDataProperty']['GeocoderMetaData']['text']
-            self.CurrentLongitude, self.CurrentLattitude = map(float, toponym['GeoObject']['Point']['pos'].split())
-            self.LEAddress.setText(toponym_address)
-            self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, True, True)
+        try:
+            response = requests.get(search_server, params=search_params)
+            json_response = response.json()
+            points = []
+            for toponym in json_response['features']:
+                points.append(','.join(list(map(str, toponym['geometry']['coordinates']))) + ',pm2bll')
+            self.CurrentLongitude = float(points[0].split(',')[0])
+            self.CurrentLattitude = float(points[0].split(',')[1])
+            self.search_with_coords(self.CurrentLongitude, self.CurrentLattitude, points)
             return True
         except:
             return None
+
--- main.py
+++ main.py
@@ -8,5 +8,3 @@ if __name__ == '__main__':
     wnd = MainForm()
     wnd.show()
     sys.exit(app.exec())
-
-os.remove('map.png')
